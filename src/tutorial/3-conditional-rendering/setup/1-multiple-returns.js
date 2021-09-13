d8667ef8cb720ddf45aefe33eab63eb92cfe4f12
luisluft <luberluft@gmail.com>
2021-09-13 09:01:03 -0400

    change the page depending on the result of asynchronous api response

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from "react";
-const url = "https://api.github.com/users/QuincyLarson";
+const url = "https://api.github.com/users/QuincyLarsons";
 
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
-  if (loading) return <h2>Loading...</h2>;
-  else return <h2>multiple returns</h2>;
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 299)
+          return response.json();
+        else {
+          setLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+      })
+      .catch((error) => console.log("error :", error));
+  }, []);
+
+  if (loading)
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+
+  if (isError)
+    return (
+      <div>
+        <h1>Error!</h1>
+      </div>
+    );
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
