0293c59123a730d41bf60fb8c9afcaaaf70d9039
luisluft <luberluft@gmail.com>
2021-09-13 10:02:50 -0400

    learn ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("test");
+  const [isError, setIsError] = useState(false);
+
   const firstValue = text || "hello world";
   const secondValue = text && "hello world";
 
@@ -12,6 +14,10 @@ const ShortCircuit = () => {
       {/* if statements here will throw error */}
       <h1>OR short circuit: {firstValue}</h1>
       <h1>AND short circuit: {secondValue}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError ? <h1>Error...</h1> : <h1>No errors!</h1>}
     </>
   );
 };
