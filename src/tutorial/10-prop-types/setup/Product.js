0a3dc1d7af523add75e731d4c6675a89844965be
luisluft <luberluft@gmail.com>
2021-10-01 08:06:01 -0400

    refactor code for different version of default values

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import defaultImage from "../../../assets/default-image.jpeg";
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  const defaultName = "unknown product";
   return (
     <article className="product">
-      <img src={image.url} alt={name} />
-      <h4>{name}</h4>
-      <p>${price}</p>
+      <img src={url || defaultImage} alt={name || defaultName} />
+      <h4>{name || defaultName}</h4>
+      <p>${price || 3.99}</p>
     </article>
   );
 };
@@ -18,10 +20,10 @@ Product.propTypes = {
   price: PropTypes.number.isRequired,
 };
 
-Product.defaultProps = {
-  name: "default name",
-  price: 3.99,
-  image: { url: defaultImage },
-};
+// Product.defaultProps = {
+//   name: "default name",
+//   price: 3.99,
+//   image: { url: defaultImage },
+// };
 
 export default Product;
